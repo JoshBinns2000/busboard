c121903c789351b7917919481e86f1e7f31d402b
Joshua Binns <Joshua.Binns@softwire.com>
2023-09-22 11:42:36 +0100

    Basic get request added

--- busboard/busboard/Program.cs
+++ busboard/busboard/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using RestSharp;
+
+const string baseUrl = "https://api.tfl.gov.uk/StopPoint";
+var options = new RestClientOptions(baseUrl);
+var client = new RestClient(options);
+
+const string stopCode = "490008660N";
+
+var request = new RestRequest($"/{stopCode}/Arrivals");
+var response = await client.GetAsync(request);
+Console.WriteLine(response.Content);
